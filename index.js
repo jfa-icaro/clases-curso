d80667565642d4f0ed99cbf299872fbf37202144
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2021-04-13 22:57:41 -0300

    segundo commit despues de terminar - clase 4

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-let nombre = document.getElementById("nombre");
+const contenido1 = document.getElementById("contenido1");
+const contenido2 = document.getElementById("contenido2");
+const contenido3 = document.getElementById("contenido3");
 
-nombre.innerText = "Juan Aguirre";
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+contenido1.classList.add("show");
+
+link1.addEventListener("click", () => {
+	contenido1.classList.add("show");
+	contenido2.classList.remove("show");
+	contenido3.classList.remove("show");
+});
+
+link2.addEventListener("click", () => {
+	contenido2.classList.add("show");
+	contenido1.classList.remove("show");
+	contenido3.classList.remove("show");
+});
+
+link3.addEventListener("click", () => {
+	contenido3.classList.add("show");
+	contenido1.classList.remove("show");
+	contenido2.classList.remove("show");
+});
+
+// --------------------------
+
+const links = Array.from(document.getElementsByClassName("link"));
+const contenidos = Array.from(document.getElementsByClassName("contenido"));
+
+links.forEach((link, index) => {
+	link.addEventListener("click", () => {
+		contenidos.forEach((contenido) => {
+			contenido.classList.remove("show");
+		});
+		contenidos[index].classList.add("show");
+	});
+});
